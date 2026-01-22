e0911c9ef22968590f91c3f2675074b77f8a65ca
mizanmahi <mizanmahi24@gmail.com>
2026-01-22 22:13:07 +0600

    update main.go to demonstrate variable declaration and initialization

--- main.go
+++ main.go
@@ -3,5 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, world!")
+
+	// name := "Mizan"
+	// name string := "Mizan" // wrong way
+
+	// var name string = "Mizan"
+	// var name = "mizan"
+
+	// grouped variables
+	var (
+		name string = "Mizan"
+		age  int    = 30
+	)
+
+	fmt.Println(name)
+	fmt.Println(age)
+
 }

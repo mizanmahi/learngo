1d5c3ebe572cae121c40376034ca935f07ed1d19
mizanmahi <mizanmahi24@gmail.com>
2026-01-28 01:36:23 +0600

    refactor main.go to remove unused makeCoffee function and enhance IIFE demonstration

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import "fmt"
 
-<<<<<<< HEAD
 func main() {
 	sugar := 2
 
@@ -11,26 +10,6 @@ func main() {
 		sugar := 3 // modify
 		fmt.Printf("Making %s with %d spoon of sugar \n", coffee, sugar)
 		fmt.Println("Value of inner sugar", sugar)
-=======
-// func makeCoffee(kind string) (string, int) { // multiple return values
-// 	price := 25
-// 	coffee := fmt.Sprintf("%s Coffee!", kind)
-// 	return coffee, price
-// }
-
-func makeCoffee(kind string) (coffee string, price int) { // named return value
-	price = 25
-	coffee = fmt.Sprintf("%s Coffee!", kind)
-	return
-}
-
-func main() {
-	myCoffee, myBill := makeCoffee("black")
-	myCoffee2, myBill2 := makeCoffee("cold")
-
-	fmt.Printf("I am having a %d$ %s \n", myBill, myCoffee)
-	fmt.Printf("I am having a %d$ %s", myBill2, myCoffee2)
->>>>>>> 48324f5 (update main.go to implement named return values in makeCoffee function and demonstrate usage in main)
 
 	}
 

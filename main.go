c898f60cc1e9090147d54397db3f1a3e54cb063a
mizanmahi <mizanmahi24@gmail.com>
2026-01-27 19:42:12 +0600

    update main.go to replace variable examples with a formatted output demonstration

--- main.go
+++ main.go
@@ -1,18 +1,14 @@
 package main
 
+import "fmt"
+
 // isAdmin := true // wrong
 
 func main() {
 
-	user_name := "alice" // snake case, correct syntax
-	isAdmin := true      // camel case, correct syntax, preferred in Go
-
-	// invalid variable names
-	// 1st-name := "bob" // hyphen not allowed
-	// 2ndName := "charlie" // cannot start with a number
-	// var float := 3.14 // 'float' is a reserved keyword
-
-	println("User Name:", user_name)
-	println("Is Admin:", isAdmin)
+	var name string = "next level"
+	rating := 4.7899434
+	formattedString := fmt.Sprintf("My name is %s and my rating is %.2f ", name, rating)
+	fmt.Println(formattedString)
 
 }

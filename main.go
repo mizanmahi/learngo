48324f51016d870eb47093352253909ba5ac5e42
mizanmahi <mizanmahi24@gmail.com>
2026-01-27 20:32:02 +0600

    update main.go to implement named return values in makeCoffee function and demonstrate usage in main

--- main.go
+++ main.go
@@ -2,13 +2,23 @@ package main
 
 import "fmt"
 
-// isAdmin := true // wrong
+// func makeCoffee(kind string) (string, int) { // multiple return values
+// 	price := 25
+// 	coffee := fmt.Sprintf("%s Coffee!", kind)
+// 	return coffee, price
+// }
+
+func makeCoffee(kind string) (coffee string, price int) { // named return value
+	price = 25
+	coffee = fmt.Sprintf("%s Coffee!", kind)
+	return
+}
 
 func main() {
+	myCoffee, myBill := makeCoffee("black")
+	myCoffee2, myBill2 := makeCoffee("cold")
 
-	var name string = "next level"
-	rating := 4.7899434
-	formattedString := fmt.Sprintf("My name is %s and my rating is %.2f ", name, rating)
-	fmt.Println(formattedString)
+	fmt.Printf("I am having a %d$ %s \n", myBill, myCoffee)
+	fmt.Printf("I am having a %d$ %s", myBill2, myCoffee2)
 
 }

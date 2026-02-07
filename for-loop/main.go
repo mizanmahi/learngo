92068ca9080c4b37eacb314ee5680835438db362
mizanmahi <mizanmahi24@gmail.com>
2026-02-07 19:34:53 +0600

    refactor main.go: clean up commented-out code and enhance loop examples

--- for-loop/main.go
+++ for-loop/main.go
@@ -8,11 +8,43 @@ func makeCoffee(coffeeNo int) {
 
 func main() {
 
-	for i := 1; i <= 5; i++ { // for initialization; condition; increment/decrement
-		makeCoffee(i)
-	}
+	// for i := 1; i <= 5; i++ { // for initialization; condition; increment/decrement
+	// 	makeCoffee(i)
+	// }
 
-	//
+	// while styled loop
+
+	// i := 1
+
+	// for i <= 5 {
+	// 	makeCoffee(i)
+	// 	i++
+	// }
+
+	// infinite loop
+	// for {
+	// 	makeCoffee(2)
+	// }
+
+	// break (break the loop), continue (skip the iteration)
+
+	// for i := 1; i <= 10; i++ {
+	// 	if i == 6 {
+	// 		break // stops the loop
+	// 	}
+	// 	makeCoffee(i)
+
+	// }
+
+	for i := 1; i <= 10; i++ {
+
+		if i%2 != 0 {
+			continue // skips the iteration
+		}
+
+		makeCoffee(i)
+
+	}
 
 }
 

4e117d5f95ccfbe19d5669d0aec59bbc4ab86506
mizanmahi <mizanmahi24@gmail.com>
2026-02-15 23:36:42 +0600

    add: implement pointer basics with example in pointer.go

---
+++ Pointers/pointer.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	a := 42
+	p := a
+
+	p = 20
+
+	fmt.Println("a:", a)
+	fmt.Println("p:", p)
+
+}
+
+// Pointer basics
+// Dereference Example
+// Modify Value Using Pointer
+// Pointer in Function
+// Nil pointer
+
+// Large data copy avoid করা যায়
+// Struct modify করা যায়
+// Performance improve হয়

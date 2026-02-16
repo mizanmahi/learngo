d63ee40163c5e34bda81a747ed41a2c857f74570
mizanmahi <mizanmahi24@gmail.com>
2026-02-17 00:52:26 +0600

    refactor struct.go: clean up commented-out code and update user struct definition

--- Struct/struct.go
+++ Struct/struct.go
@@ -2,15 +2,21 @@ package main
 
 import "fmt"
 
-type additionalInfo struct {
-	phone   int
-	address string
-}
+// type additionalInfo struct {
+// 	phone   int
+// 	address string
+// }
+
+// type user struct {
+// 	name     string
+// 	email    string
+// 	metaInfo additionalInfo
+// }
 
 type user struct {
-	name     string
-	email    string
-	metaInfo additionalInfo
+	name string
+	age  int
+	role string
 }
 
 func main() {
@@ -28,16 +34,31 @@ func main() {
 
 	// fmt.Println(user1)
 
-	john := user{
-		name:  "john",
-		email: "john@mail.com",
-		metaInfo: additionalInfo{
-			phone:   017273434,
-			address: "165/1, Rampura",
-		},
+	// john := user{
+	// 	name:  "john",
+	// 	email: "john@mail.com",
+	// 	metaInfo: additionalInfo{
+	// 		phone:   017273434,
+	// 		address: "165/1, Rampura",
+	// 	},
+	// }
+
+	// fmt.Printf("%+v", john)
+
+	newUser := func(name string, age int, role string) user {
+		if age <= 0 {
+			age = 18
+		}
+
+		return user{
+			name: name,
+			age:  age,
+			role: role,
+		}
 	}
 
-	fmt.Printf("%+v", john)
+	jamal := newUser("Jamal", -25, "student")
+	fmt.Println(jamal)
 
 }
 
@@ -48,4 +69,6 @@ func main() {
 // Accessing struct fields
 
 // embedding struct
+// constructor function
+// function fields in struct
 // receiver function

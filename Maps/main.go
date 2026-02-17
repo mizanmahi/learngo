41cebdc5a09830b268af8d6b492bd469e4bedd50
mizanmahi <mizanmahi24@gmail.com>
2026-02-18 01:13:42 +0600

    add: create main.go with initial map setup and comments for map operations

---
+++ Maps/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var myMap map[string]int
+
+	myMap["user1Phone"] = 019
+
+	
+
+}
+
+// crating map (using  make and literal)
+// accessing map values
+// adding new key-value pair to map
+// deleting key-value pair from map
+// iterating over map
+// map of struct

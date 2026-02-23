b72ecd3f6d2ed105ec8f4400b2db726f6fa528a9
mizanmahi <mizanmahi24@gmail.com>
2026-02-23 23:06:00 +0600

    add: implement payment package with PaymentMethod interface and Bkash/Nagad payment methods

--- main.go
+++ main.go
@@ -1,55 +1,9 @@
-package main
+package main // executable package
 
-import "fmt"
-
-type PaymentMethod interface {
-	pay(amount float64)
-}
-
-type Bkash struct {
-	apiKey string
-}
-
-type Nagad struct {
-	apiKey string
-}
-
-func (bk *Bkash) pay(amount float64) {
-	// actual payment logic
-	fmt.Printf("Paying %.2f tk with Bkash\n", amount)
-}
-func (ng *Nagad) pay(amount float64) {
-	// actual payment logic
-	fmt.Printf("Paying %.2f tk with Nagad", amount)
-}
-
-type PaymentService struct {
-	method PaymentMethod
-}
-
-func NewPaymentService(method PaymentMethod) *PaymentService {
-	return &PaymentService{
-		method: method,
-	}
-}
-
-func NewNagad(apiKey string) *Nagad {
-	return &Nagad{
-		apiKey: apiKey,
-	}
-}
-
-func (ps PaymentService) checkout() {
-	ps.method.pay(10.0)
-}
-
-type MockPaymentMethod struct {
-}
-
-func (mockPM MockPaymentMethod) pay(amount float64) {
-	// test logic
-	fmt.Println("testing payment method")
-}
+import (
+	"learngo/payment"
+	"learngo/test"
+)
 
 func main() {
 
@@ -59,9 +13,11 @@ func main() {
 
 	// nagad := NewNagad("safsfsf")
 
-	mockPm := MockPaymentMethod{}
+	mockPm := test.MockPaymentMethod{}
 
-	paymentService1 := NewPaymentService(mockPm)
-	paymentService1.checkout()
+	paymentService1 := payment.NewPaymentService(mockPm)
+	paymentService1.Checkout()
 
 }
+
+// module = bunch of packages
